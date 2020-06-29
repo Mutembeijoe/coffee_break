c2097ab0135121aabe5a18d308ad27a36f44fdcf
mutembeijoe <mutembeijoe@gmail.com>
2020-06-29 14:53:17 +0300

    adds signIn and Register Forms

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,14 +1,14 @@
 import 'package:coffee_break/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   final Function toggleView;
-  SignIn({this.toggleView});
+  Register({this.toggleView});
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   String email;
   String password;
@@ -18,14 +18,14 @@ class _SignInState extends State<SignIn> {
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[300],
-        title: Text("Sign In"),
+        title: Text("Sign Up"),
         actions: <Widget>[
           FlatButton.icon(
               onPressed: () {
                 widget.toggleView();
               },
               icon: Icon(Icons.person),
-              label: Text("Sign Up")),
+              label: Text("Sign In"))
         ],
       ),
       body: Container(
@@ -53,7 +53,7 @@ class _SignInState extends State<SignIn> {
                   },
                   color: Colors.brown[400],
                   child: Text(
-                    "Sign In",
+                    "Sign Up",
                     style: TextStyle(color: Colors.white),
                   ),
                 )
